bfaa975d6af2dafeab3893fe777832954549255b
AngelMzno <angelmanzano1994@gmail.com>
2025-02-13 13:38:52 -0600

    py server

---
+++ tcp_echo_server.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = "0.0.0.0"  # Listen on all available interfaces
+PORT = 57777      # Port to listen on
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print('Received', repr(data.decode()))
+            conn.sendall(data)
